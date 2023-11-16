1c47e20c2702983fa0b9d19efa3250024df2ecd7
diegolopez2002 <143581969+diegolopez2002@users.noreply.github.com>
2023-11-15 23:57:43 -0500

    Update parser.ml

--- discussions/d8_parsing/src/parser.ml
+++ discussions/d8_parsing/src/parser.ml
@@ -1,22 +1,11 @@
-open Lexer
+open LccTypes 
 
-(* Types *)
-type expr =
-| Int of int
-| Plus of expr * expr
-| Mult of expr * expr
-
-(* Provided helper function - takes a token list and an exprected token.
- * Handles error cases and returns the tail of the list *)
-let match_token (toks : token list) (tok : token) : token list =
+let match_token (toks : 'a list) (tok : 'a) : 'a list =
   match toks with
-  | [] -> raise (Failure(string_of_token tok))
+  | [] -> raise (Failure("List was empty"))
   | h::t when h = tok -> t
   | h::_ -> raise (Failure( 
-      Printf.sprintf "Expected %s from input %s, got %s"
-        (string_of_token tok)
-        (string_of_list string_of_token toks)
-        (string_of_token h)
+      Printf.sprintf "Token passed in does not match first token in list"
     ))
 
 let lookahead toks = match toks with
@@ -24,39 +13,8 @@ let lookahead toks = match toks with
   | _ -> raise (Failure("Empty input to lookahead"))
 
 
-(* Parses a token list. *)
-let rec parser (toks : token list) : expr =
-  let (t, exp) = parse_S toks in
-  if t <> [Tok_EOF] then
-    raise (Failure "did not reach EOF")
-  else
-    exp
+(* Write your code below *)
 
-(* Parses the S rule. *)
-and parse_S toks = 
-  let (t, m) = parse_M toks in
-  match lookahead t with
-  | Tok_Plus -> let t' = match_token t Tok_Plus in
-                let (t'', s) = parse_S t' in
-                (t'', Plus (m, s))
-  | _ -> t, m
+let parse_lambda toks = failwith ("Unimplemented")
 
-(* Parses the M rule. *)
-and parse_M toks =
-  let (t, n) = parse_N toks in
-  match lookahead t with
-  | Tok_Mult -> let t' = match_token t Tok_Mult in
-                let (t'', m) = parse_M t' in
-                (t'', Mult (n, m))
-  | _ -> t, n
-            
-(* Parses the N rule. *)
-and parse_N toks =
-  match lookahead toks with
-  | Tok_Int i -> let t = match_token toks (Tok_Int i) in
-                 (t, Int i)
-  | Tok_LParen -> let t = match_token toks Tok_LParen in
-                  let (t', s) = parse_S t in
-                  let t'' = match_token t' Tok_RParen in
-                  (t'', s)
-  | _ -> failwith "parse_N failed"
+let parse_engl toks = failwith ("Unimplemented")
